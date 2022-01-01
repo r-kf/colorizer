9dabe4b81d3683fd60166041b7229896817c3fe9
r-kf <86322504+r-kf@users.noreply.github.com>
2022-01-01 23:27:38 +0100

    Implement colorize function

---
+++ get_models.sh
@@ -0,0 +1,3 @@
+wget https://github.com/richzhang/colorization/blob/caffe/models/colorization_deploy_v2.prototxt
+wget http://eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2.caffemodel
+wget https://github.com/richzhang/colorization/raw/master/colorization/resources/pts_in_hull.npy
