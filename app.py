f134ee518f59facb3a1b36bc128132e94ecf926b
r-kf <86322504+r-kf@users.noreply.github.com>
2022-01-06 21:55:10 +0100

    Add save button

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from tkinter import filedialog
 import tkinter.font as tkFont
 import PyPDF2
 from PIL import Image, ImageTk
-from tkinter.filedialog import askopenfile
+from tkinter.filedialog import askopenfile, asksaveasfilename
 from cv2 import FileNode_NAMED
+import os
 
 from numpy.core.fromnumeric import resize
 
@@ -16,7 +17,8 @@ class App:
     def __init__(self, root):
 
         #setting title
-        root.title("undefined")
+        root.title("colorizer")
+
         #setting window size
         width=731
         height=501
@@ -28,32 +30,32 @@ class App:
 
         upload_button=tk.Button(root)
         upload_button["bg"] = "#c0c0c0"
-        ft = tkFont.Font(family='Times',size=10)
+        ft = tkFont.Font(family='Arial Bold',size=10)
         upload_button["font"] = ft
         upload_button["fg"] = "#000000"
         upload_button["justify"] = "center"
         upload_button["text"] = "UPLOAD"
-        upload_button.place(x=560,y=30,width=131,height=33)
+        upload_button.place(x=560,y=30,width=130,height=30)
         upload_button["command"] = self.upload
 
-        GButton_853=tk.Button(root)
-        GButton_853["bg"] = "#c0c0c0"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_853["font"] = ft
-        GButton_853["fg"] = "#000000"
-        GButton_853["justify"] = "center"
-        GButton_853["text"] = "Button"
-        GButton_853.place(x=560,y=80,width=133,height=30)
-        GButton_853["command"] = self.GButton_853_command
+        save_button=tk.Button(root)
+        save_button["bg"] = "#c0c0c0"
+        ft = tkFont.Font(family='Arial Bold',size=10)
+        save_button["font"] = ft
+        save_button["fg"] = "#000000"
+        save_button["justify"] = "center"
+        save_button["text"] = "SAVE"
+        save_button.place(x=560,y=130,width=130,height=30)
+        save_button["command"] = self.save
 
         generate_button=tk.Button(root)
         generate_button["bg"] = "#c0c0c0"
-        ft = tkFont.Font(family='Times',size=10)
+        ft = tkFont.Font(family='Arial Bold',size=10)
         generate_button["font"] = ft
         generate_button["fg"] = "#000000"
         generate_button["justify"] = "center"
         generate_button["text"] = "GENERATE"
-        generate_button.place(x=560,y=130,width=132,height=31)
+        generate_button.place(x=560,y=80,width=130,height=30)
         generate_button["command"] = self.generate
 
         img_input = ImageTk.PhotoImage(Image.open("resources/input_icon.png").resize((343, 285)))
@@ -85,8 +87,17 @@ class App:
         input_label.place(x=10,y=190,width=343,height=285)
 
 
-    def GButton_853_command(self):
+    def save(self):
         print("command")
+        
+        img = self.generate()
+
+        imgpil = ImageTk.getimage(img)
+        if imgpil.mode != "RGB":
+            imgpil = imgpil.convert("RGB")
+            
+        imgpil.save(os.path.join("/examples", asksaveasfilename(initialdir = "/", filetypes = (('JPEG', ('*.jpg','*.jpeg','*.jpe','*.jfif')),('PNG', '*.png')))), "JPEG" )
+
 
 
     def generate(self):
@@ -100,6 +111,8 @@ class App:
         output_label = Label(image=img, bg="white")
         output_label.image = img
         output_label.place(x=370,y=190,width=343,height=285)
+
+        return img
 
 
 
